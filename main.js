fa6029606132cf3c6fbbc6e5fc687e6a9d714e88
daniela <d.ortizkosobucki@gmail.com>
2022-06-01 10:16:17 -0400

    all done - minus styling

--- main.js
+++ main.js
@@ -10,42 +10,44 @@ for (let customer of customers) {
     customerElement.classList.add("customer")
 
     let contentElement = document.createElement("article")
-    contentElement.classList.add("box")
+
 
     //customer image
     let pictureElement = document.createElement("img")
     pictureElement.src = `${customer.picture.thumbnail}`
     pictureElement.alt = "Customer Thumbnail"
-    pictureElement.classList.add("img")
+
     contentElement.appendChild(pictureElement)
     customerElement.appendChild(contentElement)
 
     //full name
     let nameElement = document.createElement("h2")
-    nameElement.classList.add("h2")
     nameElement.innerText = `Name: ${customer.name.title} ${customer.name.first} ${customer.name.last}`
+    nameElement.classList.add("name")
     contentElement.appendChild(nameElement)
+
+
+
     //address
     let locationElement = document.createElement("div")
-    locationElement.classList.add("box")
     locationElement.innerText = `Address: ${customer.location.street.number} ${customer.location.street.name}, ${customer.location.city}, ${customer.location.state}, ${customer.location.postcode}`
     contentElement.appendChild(locationElement)
 
     //phone number
     let phoneElement = document.createElement("div")
-    phoneElement.classList.add("box")
+
     phoneElement.innerText = `Cell Phone: ${customer.cell}`
     contentElement.appendChild(phoneElement)
 
     //birthday
     let dobElement = document.createElement("div")
-    dobElement.classList.add("box")
+
     dobElement.innerText = `DOB: ${customer.dob.date}`
     contentElement.appendChild(dobElement)
 
     //customer dates
     let registeredElement = document.createElement("div")
-    registeredElement.classList.add("box")
+
     registeredElement.innerText = `Registered: ${customer.registered.date}`
     contentElement.appendChild(registeredElement)
 
